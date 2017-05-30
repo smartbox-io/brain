3feab44b1b682a6cc30c6fcc5f388441544a429d
Rafael Fernández López <ereslibre@ereslibre.es>
2017-05-30 23:04:50 +0200

    Improve discovery controller

--- app/controllers/cluster_api/v1/cells/discovery_controller.rb
+++ app/controllers/cluster_api/v1/cells/discovery_controller.rb
@@ -3,10 +3,16 @@ class ClusterApi::V1::Cells::DiscoveryController < ClusterTokenlessApplicationCo
   skip_before_action :load_cell, only: :create
 
   def create
-    cell = Cell.find_or_create_by(uuid: discovery_params[:uuid]) do |cell|
-      cell.fqdn = discovery_params[:fqdn]
+    cell = Cell.find_or_create_by(uuid: params[:cell][:uuid]) do |cell|
+      cell.fqdn = params[:cell][:fqdn]
       cell.ip_address = request.remote_ip
       cell.status = :discovered
+    end
+    params[:cell][:volumes].each do |mountpoint, volume_information|
+      cell.volumes.find_or_create_by(mountpoint: mountpoint) do |volume|
+        volume.total_capacity = volume_information[:total_capacity]
+        volume.available_capacity = volume_information[:available_capacity]
+      end
     end
     render json: {
              cell: {
@@ -15,12 +21,6 @@ class ClusterApi::V1::Cells::DiscoveryController < ClusterTokenlessApplicationCo
                ip_address: cell.ip_address
              }
            }
-  end
-
-  private
-
-  def discovery_params
-    params.require(:cell).permit(:uuid, :fqdn)
   end
 
 end
