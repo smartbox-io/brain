a833ff93c19381a71a9c1ea51945ac0dcb57d6af
Rafael Fernández López <ereslibre@ereslibre.es>
2017-06-08 20:45:41 +0200

    Update envvar names

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:6379/" }
+  config.redis = { url: "redis://#{ENV.fetch("REDIS_SERVICE_HOST") { "localhost" }}:#{ENV.fetch("REDIS_SERVICE_PORT") { "6379" }}/" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:6379/" }
+  config.redis = { url: "redis://#{ENV.fetch("REDIS_SERVICE_HOST") { "localhost" }}:#{ENV.fetch("REDIS_SERVICE_PORT") { "6379" }}/" }
 end
