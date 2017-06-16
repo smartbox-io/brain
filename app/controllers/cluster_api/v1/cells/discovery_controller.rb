eadc6d67338e8b4d58053efbf9e2b936803e2b4a
Rafael Fernández López <ereslibre@ereslibre.es>
2017-06-16 22:02:22 +0200

    Update cell facts when it registers

--- app/controllers/cluster_api/v1/cells/discovery_controller.rb
+++ app/controllers/cluster_api/v1/cells/discovery_controller.rb
@@ -3,17 +3,19 @@ class ClusterApi::V1::Cells::DiscoveryController < ClusterTokenlessApplicationCo
   skip_before_action :load_cell, only: :create
 
   def create
-    cell = Cell.find_or_create_by(uuid: params[:cell][:uuid]) do |cell|
+    cell = Cell.find_or_initialize_by(uuid: params[:cell][:uuid]) do |cell|
+      cell.status = :discovered
+    end
+    cell.tap do |cell|
       cell.fqdn = params[:cell][:fqdn]
       cell.ip_address = request.remote_ip
       cell.public_ip_address = params[:cell][:public_ip_address]
-      cell.status = :discovered
-    end
+    end.save
     params[:cell][:volumes].each do |mountpoint, volume_information|
-      cell.volumes.find_or_create_by(mountpoint: mountpoint) do |volume|
+      cell.volumes.find_or_initialize_by(mountpoint: mountpoint).tap do |volume|
         volume.total_capacity = volume_information[:total_capacity]
         volume.available_capacity = volume_information[:available_capacity]
-      end
+      end.save
     end
     render json: {
              cell: {
