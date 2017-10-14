3cab7b77c43d26568ea9ee72e0645a9e0a224d95
Rafael Fernández López <ereslibre@ereslibre.es>
2017-10-14 16:54:52 +0200

    Init specs

--- config/application.rb
+++ config/application.rb
@@ -18,24 +18,28 @@ require "ipaddr"
 # you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-class Srv::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 5.1
+# rubocop:disable Style/ClassAndModuleChildren
+module Srv
+  class Srv::Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-  # Only loads a smaller set of middleware suitable for API only apps.
-  # Middleware like session, flash, cookies can be added back manually.
-  # Skip views, helpers and assets when generating a new resource.
-  config.api_only = true
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
 
-  config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :sidekiq
 
-  config.generators do |g|
-    g.orm :active_record
+    config.generators do |g|
+      g.orm :active_record
+    end
+
+    config.action_dispatch.trusted_proxies = ["127.0.0.1", "::1"].map { |proxy| IPAddr.new proxy }
   end
-
-  config.action_dispatch.trusted_proxies = ["127.0.0.1", "::1"].map { |proxy| IPAddr.new proxy }
 end
+# rubocop:enable Style/ClassAndModuleChildren
